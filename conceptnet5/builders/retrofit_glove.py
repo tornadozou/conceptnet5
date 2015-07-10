dd21435f5acdcf39cfd9cd384a87a9d52f0feac1
Rob Speer <rob@luminoso.com>
2015-07-10 18:57:46 -0400

    Put back my original re-implementation of retrofitting.
    Yes, the one with the unexplained +10 and -0.5 and normalizing after
    every matrix product. The next step is to figure out which of these
    absurd things we can leave out and still get good results.

--- conceptnet5/builders/retrofit_glove.py
+++ conceptnet5/builders/retrofit_glove.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy import sparse
-
+from collections import defaultdict
 from sklearn.preprocessing import normalize
+from conceptnet5.builders.assoc_to_vector_space import negate_concept
 
 
 class SparseMatrixBuilder:
@@ -37,6 +38,8 @@ def load_conceptnet(filename, labels, verbose=True, offset=1e-9):
         print("Loading sparse associations")
 
     # Add pairwise associations
+    totals = defaultdict(float)
+
     with open(filename, encoding='utf-8') as infile:
         for line in infile:
             line = line.rstrip()
@@ -46,8 +49,21 @@ def load_conceptnet(filename, labels, verbose=True, offset=1e-9):
                 index2 = labels.add(concept2)
                 value = float(value_str)
 
+                totals[concept1] += value
+                totals[concept2] += value
                 mat[index1, index2] = value
                 mat[index2, index1] = value
+
+    if verbose:
+        print("Creating self-loops")
+
+    for index1, concept in enumerate(labels):
+        mat[index1, index1] = totals[concept] + 10
+        neg = negate_concept(concept)
+        if neg in labels:
+            index2 = labels.index(neg)
+            mat[index1, index2] = -0.5
+            mat[index2, index1] = -0.5
 
     if verbose:
         print("Building sparse matrix")
@@ -55,8 +71,8 @@ def load_conceptnet(filename, labels, verbose=True, offset=1e-9):
     return mat.tocsr(shape=(len(labels), len(labels)))
 
 
-def retrofit(vectors, sparse_matrix, labels,
-             iterations=10, verbose=True, normalize_intermediate=False):
+def retrofit(vectors, sparse_matrix, labels, orig_weight=1,
+             iterations=20, verbose=True, normalize_intermediate=False):
     """
     Updates the word vectors contained in `dense_file` using the association
     contained in `sparse_file` and writes the new vectors to `output_file`.
@@ -66,9 +82,6 @@ def retrofit(vectors, sparse_matrix, labels,
     A larger `offset` causes vectors with small magnitudes to be normalized
     into vectors with magnitudes less than 1.
     """
-
-    # length of glove vectors
-    vec_len = len(vectors[0])
     orig_vecs = np.copy(vectors)
 
     if verbose:
@@ -76,16 +89,16 @@ def retrofit(vectors, sparse_matrix, labels,
 
     for iter in range(iterations):
         if verbose:
-            print("%d/10" % (iter + 1))
+            print("%d/%d" % (iter + 1, iterations))
 
         vectors = sparse_matrix.dot(vectors)
 
-        vectors += orig_vecs
-        vectors /= 2
-
         if normalize_intermediate:
-            print("%s normalizing intermediate"%normalize_intermediate)
-            normalize(vectors, norm=normalize_intermediate, axis=0, copy=False)
+            print("%s normalizing intermediate" % normalize_intermediate)
+            normalize(vectors, norm=normalize_intermediate, axis=1, copy=False)
+
+        vectors += orig_vecs * orig_weight
+        vectors /= (1 + orig_weight)
 
         if verbose:
             print("Average diff: %s" % np.mean(np.abs(vectors - orig_vecs)))
--- evaluation/retrofitting.py
+++ evaluation/retrofitting.py
@@ -15,7 +15,8 @@ def load_csr_matrix(filename):
     matrix = np.load(filename)
     return sparse.csr_matrix((matrix['data'], matrix['indices'], matrix['indptr']), shape=matrix['shape'])
 
-def gen_assoc(normalize_sparse=True, normalize_vectors='l1', retrofit_vectors=False, normalize_intermediate=False):
+def gen_assoc(normalize_sparse=True, normalize_vectors='l1', retrofit_vectors=False,
+              retrofit_orig_weight=1., normalize_intermediate=False):
 
     print("Loading labels")
     labels = pickle.load(open('/data/retrofitting/glove_labels.pickle', mode='rb'))
@@ -25,20 +26,22 @@ def gen_assoc(normalize_sparse=True, normalize_vectors='l1', retrofit_vectors=Fa
     vectors = np.load('/data/retrofitting/glove.npy')
 
     if normalize_vectors:
-        print("%s Normalizing vectors"%normalize_vectors)
+        print("%s Normalizing vectors" % normalize_vectors)
         vectors = normalize(vectors, norm=normalize_vectors, axis=0)
 
     if retrofit_vectors:
 
         print("Loading sparse matrix")
-        sparse = load_csr_matrix('/data/retrofitting/conceptnet_sparse.npz')
+        sparse = load_csr_matrix('conceptnet-selfloop.npz')
 
         if normalize_sparse:
             #must be l1 normalized or diverges
             print("l1 Normalizing sparse matrix")
             sparse = normalize(sparse, norm='l1', axis=1)
 
-        vectors = retrofit(vectors, sparse, labels, normalize_intermediate=normalize_intermediate)
+        vectors = retrofit(vectors, sparse, labels,
+                           normalize_intermediate=normalize_intermediate,
+                           orig_weight=retrofit_orig_weight)
 
     print("Normalizing associations")
 
@@ -47,7 +50,7 @@ def gen_assoc(normalize_sparse=True, normalize_vectors='l1', retrofit_vectors=Fa
     return AssocSpace(vectors, np.ones(vectors.shape[1]), labels, assoc=assoc)
 
 def main():
-    assoc = gen_assoc()
+    assoc = gen_assoc(normalize_vectors=False, retrofit_vectors=True, normalize_sparse=False, normalize_intermediate='l2')
     wordsim.test(assoc, standardize=True)
     #analogy.test(assoc, standardize=True) # slow
 
